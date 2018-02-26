3411fa086558e1217947d5f4befc1fa9937bf60f
Andrew <andrew.toghill@alces-software.com>
2018-02-26 16:14:26 +0000

    Moved their classes up a level, getting rid of the need for the module

--- lib/adminware/config.rb
+++ lib/adminware/config.rb
@@ -2,74 +2,62 @@ require 'yaml'
 require 'fileutils'
 
 module Adminware
-  module ConfigFile
-    #Returns/Creates the config object
-    def self.return
-      @config ||= Config::new
+  class Config
+    #Set default values for config settings
+    DEFAULT_JOBDIR = "jobs/"
+    DEFAULT_LOGFILE = "logs/adminware.log"
+    DEFAULT_STATEFILE = "var/state.yaml"
+
+    #Load the config file
+    def initialize
+      @path = File.expand_path(File.dirname(__FILE__)) 
+      configfile = File.join(@path, '../../etc', 'config.yml')
+      @config = YAML.load_file(configfile)
+
+      #If no path given in config then set them to their defaults
+      @jobdir = @config['jobdir'] ||= DEFAULT_JOBDIR
+      @jobdir = set_path(@jobdir)
+
+      @logfile = @config['logfile'] ||= DEFAULT_LOGFILE
+      @logfile = set_path(@logfile)
+
+      @statefile = @config['statefile'] ||= DEFAULT_STATEFILE
+      @statefile = set_path(@statefile)
     end
 
-    #Calls the config object
-    def self.config
-      ConfigFile::return
+    #These methods return their respective files
+    def jobdir
+      @jobdir
     end
 
-    class Config
-      #Set default values for config settings
-      DEFAULT_JOBDIR = "jobs/"
-      DEFAULT_LOGFILE = "logs/adminware.log"
-      DEFAULT_STATEFILE = "var/state.yaml"
+    def logfile
+      file_exist?(@logfile)
+      @logfile
+    end
 
-      #Load the config file
-      def initialize
-        @path = File.expand_path(File.dirname(__FILE__)) 
-        configfile = File.join(@path, '../../etc', 'config.yml')
-        @config = YAML.load_file(configfile)
+    def statefile
+     @statefile
+    end
 
-        #If no path given in config then set them to their defaults
-        @jobdir = @config['jobdir'] ||= DEFAULT_JOBDIR
-        @jobdir = set_path(@jobdir)
+    private
 
-        @logfile = @config['logfile'] ||= DEFAULT_LOGFILE
-        @logfile = set_path(@logfile)
-
-        @statefile = @config['statefile'] ||= DEFAULT_STATEFILE
-        @statefile = set_path(@statefile)
+    #Check if a file exists
+    def file_exist?(file)
+      if File.exist?(file)
+        return true
+      else
+        #Create it if necessary
+        FileUtils::touch "#{file}"
       end
+    end
 
-      #These methods return their respective files
-      def jobdir
-        @jobdir
-      end
-
-      def logfile
-        file_exist?(@logfile)
-        @logfile
-      end
-
-      def statefile
-       @statefile
-      end
-
-      private
-
-      #Check if a file exists
-      def file_exist?(file)
-        if File.exist?(file)
-          return true
-        else
-          #Create it if necessary
-          FileUtils::touch "#{file}"
-        end
-      end
-
-      #Sets the path for the given config setting
-      def set_path(path)
-        #Checks if the path is absolute or relative
-        if File.exist?(path) == false
-          path = File.join(@path,"../..", path)
-        else
-          path
-        end
+    #Sets the path for the given config setting
+    def set_path(path)
+      #Checks if the path is absolute or relative
+      if File.exist?(path) == false
+        path = File.join(@path,"../..", path)
+      else
+        path
       end
     end
   end
--- lib/adminware/log.rb
+++ lib/adminware/log.rb
@@ -2,14 +2,24 @@ require 'adminware/config'
 require 'logger'
 
 module Adminware
-  module EventLogger
-    #Returns/Creates the logger    
-    def self.return
-      @log ||= Log::new
+  class Log
+    #Configure the logger
+    def initialize
+      @config = Config::new
+      @logger = Logger.new(@config.logfile)
+      @logger.level = Logger::DEBUG
+      @logger.formatter = proc do |severity, datetime, progname, msg|
+      date_format = datetime.strftime("%d-%m-%Y %H:%M:%S")
+        "[#{date_format}]  #{severity.ljust(5)}: #{msg}\n"
+      end
     end
-    
-    def self.log(level, message)
-      @logger = EventLogger::return
+
+    def method_missing(m,*args,&block)
+      log.send(m,*args,&block)
+    end
+
+    #Return the logger
+    def log(level, message)
       case level
       when 'info'
         @logger.info message
@@ -17,30 +27,9 @@ module Adminware
       when 'error'
         @logger.error message
         $stderr.puts message
-      when 'debug' 
+      when 'debug'
         @logger.debug message
-      end
-    end
-
-    class Log
-      #Configure the logger
-      def initialize
-          @logger = Logger.new(ConfigFile::config.logfile)
-          @logger.level = Logger::DEBUG
-          @logger.formatter = proc do |severity, datetime, progname, msg|
-          date_format = datetime.strftime("%d-%m-%Y %H:%M:%S")
-            "[#{date_format}]  #{severity.ljust(5)}: #{msg}\n"
-          end
-      end
-
-      def method_missing(m,*args,&block)
-        log.send(m,*args,&block)
-      end
-
-      #Return the logger
-      def log
-        @logger
-      end
+      end  
     end
   end
-end    
+end
