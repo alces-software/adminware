9c0830f3730bb73250fb1a85709e9f166c93b8e2
Andrew <andrew.toghill@alces-software.com>
2018-02-26 17:26:51 +0000

    Changed config.yml to config.yaml

--- lib/adminware/config.rb
+++ lib/adminware/config.rb
@@ -11,7 +11,7 @@ module Adminware
     #Load the config file
     def initialize
       @path = File.expand_path(File.dirname(__FILE__)) 
-      configfile = File.join(@path, '../../etc', 'config.yml')
+      configfile = File.join(@path, '../../etc', 'config.yaml')
       @config = YAML.load_file(configfile)
 
       #If no path given in config then set them to their defaults
