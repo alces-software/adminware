7912a56d3a278b285bb624636e03152964eded53
Andrew <andrew.toghill@alces-software.com>
2018-02-28 14:41:37 +0000

    Cleaned up the code and altered list_all_jobs to always display all information for each job

--- lib/adminware/list.rb
+++ lib/adminware/list.rb
@@ -9,34 +9,29 @@ module Adminware
     class << self
       #Handle the input command
       def run(name, command, state)
+        @name = name
+        @command = command
+        @state = state
+        @jobdir = Adminware::config.jobdir
+
         case command
         when 'all'
-          list_all_jobs(values = false, state)
+          list_all_jobs
         when 'job'
-          if name == 'all'
-            list_all_jobs(values = true, state)
-          else
-            list_job_values(name, state)
-          end
+          list_job_values
         end
       end
 
       #List all available jobs
-      def list_all_jobs(values, state)
-        config = Adminware::config
-        jobs = Dir.entries(config.jobdir)
+      def list_all_jobs
+        jobs = Dir.entries(@jobdir)
 
         #Create the table of jobs
-        table = Terminal::Table.new do |rows|
+        table = Terminal::Table.new :headings => ['Jobs', 'Description', 'Status', 'Exit Code'] do |rows|
+          #Iterate through each job and create a row for them
           (2..(jobs.length-1)).each do |i|
-            file = YAML::load_file(File.join(config.jobdir, jobs[i], 'job.yaml'))
-            if values
-              rows.headings = ['Job', 'Description', 'Status', 'Exit Code']
-              rows << [jobs[i], file['description'], state.print[jobs[i]][:status], state.print[jobs[i]][:exit]]
-            else
-              rows.headings = ['Job', 'Description']
-              rows << [jobs[i], file['description']]
-            end
+            file = YAML::load_file(File.join(@jobdir, jobs[i], 'job.yaml'))
+            rows << [jobs[i], file['description'], @state.print[jobs[i]][:status], @state.print[jobs[i]][:exit]]
           end
           rows.style = {:alignment => :center, :padding_left => 2, :padding_right => 2}
         end
@@ -44,38 +39,38 @@ module Adminware
       end
 
       #Lists the values of a job within the state file
-      def list_job_values(name, state)
-        if job_exists?(name, state)
-          create_table(name, state)
+      def list_job_values
+        if job_exists?
+          create_table
         else exit 1 end
       end
       
       #Create the table for the given job      
-      def create_table(name, state)
-        job = YAML::load_file(File.join(Adminware::config.jobdir, name, 'job.yaml'))
+      def create_table
+        job = YAML::load_file(File.join(@jobdir, @name, 'job.yaml'))
         table = Terminal::Table.new :headings => ['Job', 'Description', 'Status', 'Exit Code'] do |rows|
-          rows << [name, job['description'], state.print[name][:status], state.print[name][:exit]]
+          rows << [@name, job['description'], @state.print[@name][:status], @state.print[@name][:exit]]
           rows.style = {:alignment => :center, :padding_left => 2, :padding_right => 2}
         end
         puts table
       end 
      
       #Checks the job exists within the state file
-      def job_exists?(name, state)
-        if state.print.include? "#{name}"
+      def job_exists?
+        if @state.print.include? "#{@name}"
           return true
         else
-          job_dir_exist?(name)
+          job_dir_exist?
         end
       end
 
       #Checks the job directory exists
-      def job_dir_exist?(name)
-        jobdir = File.join(Adminware::config.jobdir, name)
-        if Dir.exist?(jobdir)
-          puts "#{name} needs to be run at least once before it can be listed"
+      def job_dir_exist?
+        namedir = File.join(@jobdir, @name)
+        if Dir.exist?(namedir)
+          puts "#{@name} needs to be run at least once before it can be listed"
         else
-          puts "The directory for #{name} does not exist and therefore can't be run or listed"
+          puts "The directory for #{@name} does not exist and therefore can't be run or listed"
         end
       end
     end
