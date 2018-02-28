901a5d2948534f5345e31c7ddefc139cd6dac00c
Andrew <andrew.toghill@alces-software.com>
2018-02-28 11:16:29 +0000

    List commands should output as a formatted table

--- lib/adminware/list.rb
+++ lib/adminware/list.rb
@@ -2,6 +2,7 @@ require 'adminware'
 require 'adminware/config'
 require 'adminware/state'
 require 'fileutils'
+require 'terminal-table'
 
 module Adminware
   module ListCommands
@@ -19,19 +20,36 @@ module Adminware
       #List all available jobs
       def list_all_jobs
         config = Adminware::config
-        puts 'Available jobs:'
-        puts Dir.entries(config.jobdir)[2..-1]
+        jobs = Dir.entries(config.jobdir)
+
+        #Create the table of jobs
+        table = Terminal::Table.new do |rows|
+          rows << ['Available jobs']
+          rows.add_separator
+          (2..(jobs.length-1)).each do |i|
+            rows << [{:value => jobs[i], :alignment => :center}]
+          end
+        end
+        puts table
       end
 
       #Lists the values of a job within the state file
       def list_job_values(name, state)
         if job_exists?(name, state)
-          status = state.print["#{name}"][:status]
-          code = state.print["#{name}"][:exit]
-          puts "#{name}: Status = #{status},  Last Exit Code = #{code}"
+          create_table(name, state)
         else exit 1 end
       end
       
+      #Create the table for the given job      
+      def create_table(name, state)
+        table = Terminal::Table.new :headings => ['Job', 'Variable', 'Value'] do |rows|
+          rows << [name, 'Status', state.print[name][:status]]
+          rows << ['','Exit Code', state.print[name][:exit]]
+          rows.style = {:alignment => :right}
+        end
+        puts table
+      end 
+     
       #Checks the job exists within the state file
       def job_exists?(name, state)
         if state.print.include? "#{name}"
@@ -41,6 +59,7 @@ module Adminware
         end
       end
 
+      #Checks the job directory exists
       def job_dir_exist?(name)
         jobdir = File.join(Adminware::config.jobdir, name)
         if Dir.exist?(jobdir)
