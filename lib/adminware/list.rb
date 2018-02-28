b6da52971cd8c7d07bf2274da8d2f624f53d541e
Andrew <andrew.toghill@alces-software.com>
2018-02-28 14:09:14 +0000

    Added the ability to list the specifics for every job at once with adminware list --job all

--- lib/adminware/list.rb
+++ lib/adminware/list.rb
@@ -11,24 +11,34 @@ module Adminware
       def run(name, command, state)
         case command
         when 'all'
-          list_all_jobs
+          list_all_jobs(values = false, state)
         when 'job'
-          list_job_values(name, state)
+          if name == 'all'
+            list_all_jobs(values = true, state)
+          else
+            list_job_values(name, state)
+          end
         end
       end
 
       #List all available jobs
-      def list_all_jobs
+      def list_all_jobs(values, state)
         config = Adminware::config
         jobs = Dir.entries(config.jobdir)
 
         #Create the table of jobs
-        table = Terminal::Table.new :headings => ['Name', 'Description'] do |rows|
+        table = Terminal::Table.new do |rows|
           (2..(jobs.length-1)).each do |i|
             file = YAML::load_file(File.join(config.jobdir, jobs[i], 'job.yaml'))
-            rows << [jobs[i], file['description']]
+            if values
+              rows.headings = ['Job', 'Description', 'Status', 'Exit Code']
+              rows << [jobs[i], file['description'], state.print[jobs[i]][:status], state.print[jobs[i]][:exit]]
+            else
+              rows.headings = ['Job', 'Description']
+              rows << [jobs[i], file['description']]
+            end
           end
-          rows.style = {:alignment => :center}
+          rows.style = {:alignment => :center, :padding_left => 2, :padding_right => 2}
         end
         puts table
       end
