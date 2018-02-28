98b6fed7acec39bb5badee82b846ac24d0f38250
Andrew <andrew.toghill@alces-software.com>
2018-02-28 14:02:51 +0000

    Changed the naming of a list option; --name NAME => --job NAME

--- lib/adminware/cli.rb
+++ lib/adminware/cli.rb
@@ -33,7 +33,7 @@ module Adminware
         c.description = 'Lists details about jobs'
         c.example 'List values for NAME', 'adminware list --name NAME'
         c.option '-a', '--all', 'Lists all available jobs'
-        c.option '-n', '--name NAME', String, 'Specify the name of the job to list'
+        c.option '-j', '--job NAME', String, 'Specify the name of the job to list'
         c.action do |args, options|
           options.default \
             :all => false
