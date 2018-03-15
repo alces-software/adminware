32363196b6726381340a8faa7ad859efb3a38421
Andrew <andrew.toghill@alces-software.com>
2018-03-15 11:23:13 +0000

    Changed the dir_exist? and file_exist? methods to reduce any side effects and improve readability

--- lib/adminware/job.rb
+++ lib/adminware/job.rb
@@ -32,10 +32,8 @@ module Adminware
       #Checks if the job needs to run on another host
       if running_locally?
         set_script("bash #{@file}")
-      elsif @remote_dir == 'local'
-        set_script("ssh #{@host} bash #{@file}")
       else
-        set_script("ssh #{@host} bash #{File.expand_path(@config.central_jobdir)}/#{@name}/#{@command}.sh")
+        set_script("ssh #{@host} bash #{@file}")
       end
 
       #Prevents the running of commands that match the requested job's status
@@ -61,15 +59,13 @@ module Adminware
 
     #Checks to see if the directory exists for input NAME
     def dir_exist?
-      central = File.join(@config.central_jobdir, @name)
+      central = File.expand_path(File.join(@config.central_jobdir, @name))
       local = File.join(@config.local_jobdir, @name)
 
       if Dir.exist?(central)
-        @file = File.join(central, @command + '.sh')
-      elsif Dir.exist?(local)
-        @file = File.join(local, @command + '.sh')
-      elsif exist_remotely?(local)
-        @file = File.join(local, @command + '.sh')
+        set_file_path(central)
+      elsif Dir.exist?(local) || exist_remotely?(local)
+        set_file_path(local)
       else  
         @logger.log('error', "The job directory for #{@name} on #{@host} does not exist")
         false
@@ -78,16 +74,19 @@ module Adminware
 
     #Checks to see if the file exists for input COMMAND
     def file_exist?
-      if File.exist?(@file)
+      if File.exist?(@file) || exist_remotely?(@file)
         true
-      elsif exist_remotely?(@file)
-        @remote_dir = 'local'
       else
         @logger.log('error', "The #{@command} script for #{@name} on #{@host} does not exist")
         false
       end
     end
-    
+  
+    #Sets the path for the file to be run 
+    def set_file_path(directory)
+      @file = File.join(directory, @command + '.sh')
+    end
+  
     #Checks if the job's status matches the entered command
     def status_matches_command?
       if @state.status(@name) == @command then true end
