82809cf8fa9986a26806eb91409564ec119d8e4c
Andrew <andrew.toghill@alces-software.com>
2018-03-21 10:13:37 +0000

    Removed unnecessary quotation marks

--- install/alces-adminware.sh
+++ install/alces-adminware.sh
@@ -1,6 +1,6 @@
 if [ "$UID" == "0" ]; then
   if [ -d /opt/adminware/install/base.sh ]; then
-    if [ -r "$/opt/adminware/install/base.sh" ]; then
+    if [ -r /opt/adminware/install/base.sh ]; then
       if [ "${-#*/opt/adminware/install/base.sh}" != "$-" ]; then
         . /opt/adminware/install/base.sh
       else
