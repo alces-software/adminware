db97e9deab03bd3c850574f0e00c88fd57335e3a
Andrew <andrew.toghill@alces-software.com>
2018-03-28 10:48:40 +0100

    Altered so a schedule isn't made for the group itself but rather just adding to the individual schedules of each node in the group

--- lib/adminware/schedule.rb
+++ lib/adminware/schedule.rb
@@ -6,17 +6,24 @@ require 'fileutils'
 
 module Adminware
   class Schedule
-    def initialize(host)
+    attr_accessor :share_index
+    def initialize(host, group)
       @host = host
+      @group = group
       @config = Adminware.config
-      @file = File.join(Adminware.root, 'var', "#{@host}_schedule.yaml")
 
-      load_schedule
+      load_index
+      if @group.nil?
+        @file = File.join(Adminware.root, 'var', "#{@host}_schedule.yaml")
+        load_schedule
+      end
     end
 
     #Add a job to the schedule
     def add_job(name, command)
+      increment_index
       hash = {
+        :id => @index[:index],
         :job => name, 
         :status => command, 
         :scheduled => true, 
@@ -25,7 +32,19 @@ module Adminware
         :run_date => 'N/A'
       }
       @schedule.push(hash)
-      puts "\t> #{command} script for #{name} scheduled on #{@host}"
+
+      puts "\t> #{command} script for #{name} scheduled on #{@host}" unless @share_index
+    end
+
+    def add_to_individual_schedules(name, command)
+      increment_index
+      @groups[@group].each do |host|
+        schedule = Schedule.new(host, nil)
+        schedule.share_index = true
+        schedule.add_job(name, command)
+        schedule.save!
+      end
+      puts "\t> #{command} script for #{name} scheduled for #{@group} (#{@groups.count} nodes)"
     end
     
     def get_time
@@ -34,12 +53,20 @@ module Adminware
     end
 
     def save!
-      File.write(@file, @schedule.to_yaml)
+      File.write(@file, @schedule.to_yaml) 
     end
     
     #This ensures you can't schedule invalid jobs
     def valid?(name, command)
-      Job.new(name, command, @host).valid?
+      if @group.nil?
+        Job.new(name, command, @host).valid?
+      else
+        group_path = File.join(Adminware.root, 'var/groups.yaml')
+        @groups = YAML.load_file(group_path)
+        @groups[@group].each do |host|
+          Job.new(name, command, host).valid?
+        end
+      end
     end
     
     def load_array
@@ -61,6 +88,15 @@ module Adminware
     #Load the schedule file for the given host
     def load_schedule
       @schedule = YAML.load_file(@file) rescue [] 
+    end
+
+    def load_index
+      @index = YAML.load_file(File.join(Adminware.root, 'var/cache.yaml')) rescue {}
+    end
+  
+    def increment_index
+      @index.empty? ? (@index[:index] = 1) : (@index[:index] = @index[:index]+1 unless @share_index)
+      File.write(File.join(Adminware.root, 'var/cache.yaml'), @index.to_yaml)
     end
   end
 end
