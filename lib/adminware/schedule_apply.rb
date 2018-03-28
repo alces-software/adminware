c897d063da04f23d52943f4dfa5dc2e5ccdc1e70
Andrew <andrew.toghill@alces-software.com>
2018-03-28 11:09:12 +0100

    Refactored to apply group schedules on a host by host basis rather than the old system of having a schedule specifically for the group

--- lib/adminware/schedule_apply.rb
+++ lib/adminware/schedule_apply.rb
@@ -12,29 +12,28 @@ module Adminware
         @group = group
         @file = Schedule.new(host, group)
         @schedule = @file.load_array
+        @state = State.new(host) unless host.nil?
         @config = Adminware.config
 
-        get_hosts
-        find_next_job
-        run_schedule        
+        if group.nil?
+          find_next_job
+          run_schedule        
+        else
+          get_hosts
+          run_each_schedule
+        end
       end
-      
+
       def get_hosts
         @hosts = []
-        if @group.nil?
-          @hosts << @host
-          @name = @host
-        else
-          group_file = YAML.load_file(File.join(Adminware.root, 'var/groups.yaml'))
-          if group_file[@group]
-            group_file[@group].each do |host|
-              @hosts << host
-            end
-          else
-            puts "\t> The group '#{@group}' could not be found"
-            exit 1
+        group_file = YAML.load_file(File.join(Adminware.root, 'var/groups.yaml'))
+        if group_file[@group]
+          group_file[@group].each do |host|
+            @hosts << host
           end
-          @name = @group
+        else
+          puts "\t> The group '#{@group}' could not be found"
+          exit 1
         end
       end
 
@@ -51,12 +50,11 @@ module Adminware
       #Iterates through the schedule and runs queued jobs in order
       def run_schedule
         if @job_number.nil?
-          puts "\t> No jobs currently scheduled for #{@name}"
+          puts "\t> No jobs currently scheduled for #{@host}"
         else
           (@job_number..(@schedule.length-1)).each do |n|
             @job_name = @schedule[n][:job]
             @status = @schedule[n][:status]
-            @id = @schedule[n][:id]
  
             if @schedule[n][:scheduled] 
               #Stop further jobs in schedule if current fails
@@ -70,29 +68,22 @@ module Adminware
           end
         end
       end
+
+      def run_each_schedule
+        @hosts.each do |host|
+          ScheduleApply.run(host, nil)
+        end
+      end
       
       def save_schedule_values(n, success)
         if success
           #If ran successfully flag as no longer scheduled
           @schedule[n][:scheduled] = false
         end
-       
-        @schedule[n][:run_date] = get_time
-        @hosts.each do |host| 
-          state = YAML.load_file(File.join(@config.statedir, "#{host}_state.yaml"))
-          @schedule[n][:exit] = state[@job_name][:exit]
-          if !@group.nil?
-            file = Schedule.new(host, nil)
-            schedule = file.load_array
-            find_job_in_schedule(schedule)
-            if schedule[@n][:scheduled]
-              schedule[@n][:scheduled] = false
-              schedule[@n][:run_date] = @schedule[n][:run_date]
-              schedule[@n][:exit] = @schedule[n][:exit]
-              file.save!
-            end
-          end
-        end
+
+        state = YAML.load_file(File.join(@config.statedir, "#{@host}_state.yaml"))
+        @schedule[n][:run_date] = get_time 
+        @schedule[n][:exit] = state[@job_name][:exit]
         @file.save!
       end
       
@@ -111,14 +102,11 @@ module Adminware
 
       #Creates a job instance and attempts to run the job from the schedule
       def execute_job
-        @hosts.each do |host|
-          state = State.new(host)
-          job = Job.new(@job_name, @status, host)
-          job.state = state
-          
-          job.valid? 
-            job.run ? true : false
-        end
+        job = Job.new(@job_name, @status, @host)
+        job.state = @state
+        
+        job.valid? 
+          job.run ? true : false
       end
     end
   end
