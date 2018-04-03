2d62447f2d15880f1a160f9382119f4d3331a7bb
Andrew <andrew.toghill@alces-software.com>
2018-04-03 11:09:33 +0100

    Handles the creation of the adminware-firstrun service which executes the firstrun command

---
+++ install/firstrun.sh
@@ -0,0 +1,42 @@
+cat << EOF > /opt/adminware/lib/firstrun/bin/firstrun
+#!/bin/bash
+function fr {
+  source /etc/profile.d/alces-adminware.sh
+  echo "Running Adminware Firstrun Scripts"
+  `adminware job-run --firstrun`
+  rm -f /opt/adminware/lib/firstrun/RUN
+}
+trap fr EXIT
+EOF
+
+cat << EOF > /opt/adminware/lib/firstrun/bin/firstrun-stop
+#!/bin/bash
+/bin/systemctl disable adminware-firstrun.service
+if [ -f /adminware-firstrun.reboot ]; then
+  echo -n "Reboot flag set.. Rebooting.."
+  rm -f /adminware-firstrun.reboot
+  shutdown -r now
+fi
+EOF
+
+cat << EOF > /etc/systemd/system/adminware-firstrun.service
+[Unit]
+Description=FirstRun service
+After=network-online.target remote-fs.target
+Before=display-manager.service getting@tty1.service
+[Service]
+ExecStart=/bin/bash /opt/adminware/lib/firstrun/bin/firstrun
+Type=oneshot
+ExecStartPost=/bin/bash /opt/adminware/lib/firstrun/bin/firstrun-stop
+TimeoutSec=0
+RemainAfterExit=yes
+Environment=HOME=/root
+Environment=USER=root
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod 664 /etc/systemd/system/adminware-firstrun.service
+systemctl daemon-reload
+systemctl enable adminware-firstrun.service
+touch /opt/adminware/lib/firstrun/RUN
