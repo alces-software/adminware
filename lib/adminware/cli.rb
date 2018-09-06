8acde23560625ca387404ee5ab86ab0d056ad310
Bob Whitelock <bob.whitelock1@gmail.com>
2018-09-06 10:50:55 +0100

    Override Thor to allow defining `run` command

--- lib/adminware/cli.rb
+++ lib/adminware/cli.rb
@@ -8,6 +8,16 @@ module Adminware
   #
   # @api public
   class CLI < Thor
+    class << self
+      # Hackery. Take the run method away from Thor so that we can define a
+      # command with this name. Stolen from
+      # https://github.com/ddollar/foreman/blob/c0b178c164396d6afe6fc3874abfd22f6cddbeae/lib/foreman/cli.rb#L29,L35.
+      def is_thor_reserved_word?(word, type)
+        return false if word == "run"
+        super
+      end
+    end
+
     # Error raised by this runner
     Error = Class.new(StandardError)
 
