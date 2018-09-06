5b9f6d6ab20d5e1f4015f0abb7cd0b7e555b1e9c
Bob Whitelock <bob.whitelock1@gmail.com>
2018-09-06 11:04:17 +0100

    Make tweaks so generated test suite passes
    In particular need to change `adminware` executable run to `admin` since
    already renamed this.

--- spec/integration/run_spec.rb
+++ spec/integration/run_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe "`adminware run` command", type: :cli do
-  it "executes `adminware help run` command successfully" do
-    output = `adminware help run`
+RSpec.describe "`admin run` command", type: :cli do
+  it "executes `admin help run` command successfully" do
+    output = `admin help run`
     expected_output = <<-OUT
 Usage:
-  adminware run TOOL...
+  admin run TOOL...
 
 Options:
   -h, [--help], [--no-help]  # Display usage information
