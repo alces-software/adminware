a1a957cbd32fb2ba0b82c7ed52f61c8894bf7ef5
william <william.mccumstie@outlook.com>
2018-09-13 11:35:37 +0100

    Create an Action class to clean up the code
    Previously a has was used to encode the name and path. However this
    indicated a proper class was required

--- src/action.py
+++ src/action.py
@@ -2,21 +2,26 @@
 import glob
 import os
 
+class Action:
+    def __init__(self, path):
+        self.path = path
+
+    def name(self):
+        return os.path.basename(os.path.dirname(self.path))
+
+
 def add_actions(click_group, namespace):
     actions = __glob_actions(namespace)
-    for action in sorted(actions):
+    for action in actions:
         def _temp_f(action=action):
             print(action)
-        _temp_f.__name__ = action
+        _temp_f.__name__ = action.name()
         locals()[action] = _temp_f
         del _temp_f
         locals()[action] = click_group.command(help='TODO')(locals()[action])
 
-
 def __glob_actions(namespace):
     parts = ['/var/lib/adminware/tools', namespace, '*/config.yaml']
     paths = glob.glob(os.path.join(*parts))
-    return dict(
-        ((os.path.basename(os.path.dirname(p)), p) for p in paths)
-    )
+    return list(map(lambda x: Action(x), paths))
 
