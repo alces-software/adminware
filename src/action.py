5bf5180470265662f7094b555ebf3041ad5df34c
william <william.mccumstie@outlook.com>
2018-09-13 11:50:12 +0100

    Move the action creation code into the class
    By moving the code into the class, it removes the need to rename the
    methods. Instead `action_func` is define multiple times in different
    instances. Only the __name__ needs to be redefined

--- src/action.py
+++ src/action.py
@@ -9,16 +9,21 @@ class Action:
     def name(self):
         return os.path.basename(os.path.dirname(self.path))
 
+    def create(self, click_group):
+        def action_func(self=self):
+            print(self.name())
+        action_func.__name__ = self.name()
+        action_func = self.__click_func(action_func, click_group)
+
+    # This method must be called after `create` to ensure the local
+    # function has been defined
+    def __click_func(self, func, click_group):
+        return click_group.command(help='TODO')(func)
 
 def add_actions(click_group, namespace):
     actions = __glob_actions(namespace)
     for action in actions:
-        def _temp_f(action=action):
-            print(action)
-        _temp_f.__name__ = action.name()
-        locals()[action] = _temp_f
-        del _temp_f
-        locals()[action] = click_group.command(help='TODO')(locals()[action])
+        action.create(click_group)
 
 def __glob_actions(namespace):
     parts = ['/var/lib/adminware/tools', namespace, '*/config.yaml']
