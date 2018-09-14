0150b0c9f4ab1171a39813ad2d0a81f874d6eec7
william <william.mccumstie@outlook.com>
2018-09-14 15:26:21 +0100

    Create a one-many relationship between Batch and Job
    This will allow a Batch to run multiple jobs across the cluster. This
    commit sets up the db and models for this purpose

--- src/db_setup.py
+++ src/db_setup.py
@@ -2,6 +2,7 @@
 from database import Base, Session, engine
 
 from models import batch
+from models import job
 
 Base.metadata.create_all(engine)
 
--- src/models/job.py
+++ src/models/job.py
@@ -1,6 +1,7 @@
 
 import datetime
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -12,8 +13,11 @@ class Job(Base):
     id = Column(Integer, primary_key=True)
     node = Column(String)
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    batch_id = Column(Integer, ForeignKey('batches.id'))
+    batch = relationship("Batch", backref="jobs")
 
 
     def __init__(self, **kwargs):
         self.node = kwargs['node']
+        self.batch = kwargs['batch']
 
