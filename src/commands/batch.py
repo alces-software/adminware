7db5f7cc077da18f091d1b3d5af34eb32370bcd3
william <william.mccumstie@outlook.com>
2018-09-17 15:48:49 +0100

    Preform the `batch-id` filtering in SQL

--- src/commands/batch.py
+++ src/commands/batch.py
@@ -26,14 +26,12 @@ def add_commands(appliance):
         nodes = ctx.obj['adminware']['nodes']
         batch_id_filter = options['batch_id']
         session = Session()
-        jobs = session.query(Job) \
-                      .filter(Job.node.in_(nodes)) \
-                      .all()
-        def job_filter(job):
-            if batch_id_filter and int(batch_id_filter) != job.batch.id:
-                return False
-            return True
-        jobs = [job for job in jobs if job_filter(job)]
+        query = session.query(Job) \
+                       .filter(Job.node.in_(nodes))
+        if options['batch_id']:
+            query = query.join(Job, Batch.jobs) \
+                         .filter(Batch.id == int(options['batch_id']))
+        jobs = query.all()
         jobs = sorted(jobs, key=lambda job: job.created_date, reverse=True)
         def table_rows():
             rows = [['Batch', 'Node', 'Command', 'Exit Code', 'Date']]
