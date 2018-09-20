8b55e8f8bc268eca5c3d2b891b921d1928293542
william <william.mccumstie@outlook.com>
2018-09-20 11:04:14 +0100

    Pass the static `Config` into the `actions`
    During the `sandbox` commands can be reran multiple times. This meant
    that the same `batch` object would be used for all the runs. This is
    obviously not ideal.
    
    Instead the static `Config` object is passed in instead. This will allow
    multiple `Batch` objects to be created with the same `Config` object

--- src/commands/open_command.py
+++ src/commands/open_command.py
@@ -2,6 +2,7 @@
 import click
 from action import ClickGlob
 from models.job import Job
+from models.batch import Batch
 
 # Note: this module cannot be called `open`, which would be consistent with
 # other command modules, as this conflicts with Python's built-in `open`
@@ -20,7 +21,8 @@ def add_commands(appliance):
 
     @ClickGlob.command(open_command, 'open')
     @click.pass_context
-    def run_open(ctx, batch):
+    def run_open(ctx, config):
+        batch = Batch(config = config.path)
         job = Job(node = ctx.obj['adminware']['node'], batch = batch)
         job.run(interactive = True)
 
