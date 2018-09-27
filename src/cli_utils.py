0cf3bba527fa9d1a195bb6aabdfa4409fd152c57
DavidMarchant <davidwjmarchant@gmail.com>
2018-09-27 12:25:29 +0100

    Remove any nodes specified more than once

--- src/cli_utils.py
+++ src/cli_utils.py
@@ -1,8 +1,10 @@
 
 import groups
 from click import ClickException
+from collections import OrderedDict
 
 def set_nodes_context(ctx, **kwargs):
+    # ctx.obj with nodes
     obj = { 'nodes' : [] }
     if kwargs['node']:
         for node in kwargs['node']:
@@ -13,4 +15,9 @@ def set_nodes_context(ctx, **kwargs):
             if not nodes:
                 raise ClickException('Could not find group: {}'.format(group))
             obj['nodes'].extend(nodes)
+    obj['nodes'] = __remove_duplicates(obj['nodes'])
     ctx.obj = { 'adminware' : obj }
+
+def __remove_duplicates(target_list):
+    # gone for the slightly more intensive method that preserves order for pretty output
+    return OrderedDict((x, True) for x in target_list).keys()
