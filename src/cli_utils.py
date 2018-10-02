be4c96f7ab8f69bf1ada932b98cf63aca0bee891
DavidMarchant <davidwjmarchant@gmail.com>
2018-10-02 14:01:00 +0100

    Change node selection to parse as genders file
    - remove regex method of expanding node ranges
    - now done via nodeattr

--- src/cli_utils.py
+++ src/cli_utils.py
@@ -1,6 +1,5 @@
 
 import groups
-import re
 from click import ClickException
 from collections import OrderedDict
 
@@ -8,11 +7,7 @@ def set_nodes_context(ctx, **kwargs):
     # populate ctx.obj with nodes
     obj = { 'nodes' : [] }
     if kwargs['node']:
-        for node in kwargs['node']:
-            if re.match(r'.*\[[0-9]+-[0-9]+\]$', node):
-                obj['nodes'] += __add_multiple_nodes(node)
-            else:
-                obj['nodes'].append(node)
+        obj['nodes'].extend(groups.expand_nodes(kwargs['node']))
     if kwargs['group']:
         for group in kwargs['group']:
             nodes = groups.nodes_in(group)
@@ -25,21 +20,3 @@ def set_nodes_context(ctx, **kwargs):
 def __remove_duplicates(target_list):
     # gone for the slightly more intensive method that preserves order for pretty output
     return OrderedDict((x, True) for x in target_list).keys()
-
-def __add_multiple_nodes(nodes):
-    prefix = re.sub(r'\[.*?$', '', nodes)
-    num_2 = re.search(r'-([0-9]+)]$', nodes).group(1)
-    num_1 = re.search(r'\[([0-9]+)-[0-9]+\]$', nodes).group(1)
-    if (num_1 > num_2) or not num_1.isdigit() or not num_2.isdigit():
-        raise ClickException('Invalid node range - {}{} to {}{}'.format(prefix, num_1, prefix, num_2))
-    else:
-        mid = ''
-        # allowing for padding 0s in the range
-        if re.match(r'^0', num_1):
-            mid = re.search(r'^(0+)', num_1).group(1)
-        nodes_list = []
-        i = int(num_1)
-        while i <= int(num_2):
-            nodes_list += [prefix + mid + str(i)]
-            i += 1
-        return nodes_list
--- src/groups.py
+++ src/groups.py
@@ -1,15 +1,27 @@
 
+# all dependancy on nodeattr is to be located in this file
 from plumbum import local
-
-__nodeattr = local['nodeattr']['-f', '/var/lib/adminware/genders']
+from tempfile import NamedTemporaryFile
 
 def list():
-    groups = __nodeattr('-l').split("\n")
+    groups = __nodeattr()('-l').split("\n")
     groups.remove('')
     return groups
 
 def nodes_in(group_name):
-    nodes = __nodeattr('-n', group_name).split("\n")
+    nodes = __nodeattr()('-n', group_name).split("\n")
     nodes.remove('')
     return nodes
 
+def expand_nodes(node_list):
+    tmp_file = NamedTemporaryFile(dir='/tmp/')
+    with open(tmp_file.name, 'w') as fil:
+        fil.write('\n'.join(node_list))
+    nodes = __nodeattr(file_path=tmp_file.name)('--expand').split("\n")
+    # above split adds trailing empty string in array so
+    del nodes[-1]
+    return nodes
+
+def __nodeattr(file_path=None):
+    if file_path: return local['nodeattr']['-f', file_path]
+    else: return local['nodeattr']['-f', '/var/lib/adminware/genders']
