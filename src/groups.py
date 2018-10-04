5d9ea147b0fe2897e16395b43e644b6447f735b8
DavidMarchant <davidwjmarchant@gmail.com>
2018-10-04 14:54:31 +0100

    Prevent crash if no valid genders file found
    Fixes #71
    
    Move more functionality to __nodeattr method & increase number of params
    in order to...
    Surround __nodeattr functionality in try-block, preventnig crash

--- src/groups.py
+++ src/groups.py
@@ -1,15 +1,16 @@
 
 # all dependancy on nodeattr is to be located in this file
-from plumbum import local
+from plumbum import local, ProcessExecutionError
 from tempfile import NamedTemporaryFile
+from click import ClickException
 
 def list():
-    groups = __nodeattr()('-l').split("\n")
+    groups = __nodeattr(arguments=['-l'])
     groups.remove('')
     return groups
 
 def nodes_in(group_name):
-    nodes = __nodeattr()('-n', group_name).split("\n")
+    nodes = __nodeattr(arguments=['-n', group_name])
     nodes.remove('')
     return nodes
 
@@ -17,10 +18,13 @@ def expand_nodes(node_list):
     tmp_file = NamedTemporaryFile(dir='/tmp/')
     with open(tmp_file.name, 'w') as f:
         f.write('\n'.join(node_list))
-    nodes = __nodeattr(file_path=tmp_file.name)('--expand').split("\n")
+    nodes = __nodeattr(file_path=tmp_file.name, arguments=['--expand'])
     # above split adds trailing empty string in array so
     del nodes[-1]
     return nodes
 
-def __nodeattr(file_path='/var/lib/adminware/genders'):
-    return local['nodeattr']['-f', file_path]
+def __nodeattr(file_path='/var/lib/adminware/genders', arguments=[], split_char="\n"):
+    try:
+        return local['nodeattr']['-f', file_path](arguments).split(split_char)
+    except ProcessExecutionError as e:
+        raise ClickException('Unable to process genders file -- {}'.format(e.stderr))
