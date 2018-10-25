af30233881e61ce44e059479d407704f6e32cb98
william <william.mccumstie@outlook.com>
2018-10-25 17:17:05 +0100

    Use scoped_session to connect to the db
    A scoped_session acts as a registry for creating thread-local sessions.
    This means the `Session` object will now return a different session for
    each thread. This is the recommended practice for dealing with
    concurrency:
    
    https://docs.sqlalchemy.org/en/latest/orm/contextual.html#unitofwork-contextual

--- src/database.py
+++ src/database.py
@@ -4,8 +4,10 @@ import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 
 engine = create_engine('sqlite:///{}database.db'.format(config.LEADER))
-Session = sessionmaker(bind=engine)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
 
 Base = declarative_base()
