d56a1ec9121ee02dd0933cf3b03c01febc825764
william <william.mccumstie@outlook.com>
2018-11-01 14:00:25 +0000

    Save the session in the finaliser
    Now that the ID is assigned upfront, the entry does not need to be saved
    before the `click.echo`. Instead it can always been done at the end

--- src/commands/run.py
+++ src/commands/run.py
@@ -82,15 +82,13 @@ def add_commands(appliance):
                 try:
                     job = local_session.merge(self.unsafe_job)
                     job.run()
-                    local_session.commit()
                     if job.exit_code == 0:
                         symbol = 'Pass'
                     else:
                         symbol = 'Failed: {}'.format(job.exit_code)
                     click.echo('ID: {}, Node: {}, {}'.format(job.id, job.node, symbol))
-                except:
-                    local_session.commit()
                 finally:
+                    local_session.commit()
                     Session.remove()
 
         session = Session()
