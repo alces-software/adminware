d913736005e187da88c7862a9529d7ef42565815
william <william.mccumstie@outlook.com>
2018-11-05 12:47:20 +0000

    Use `setdefault` to unpack the arguments
    Setdefault sets the `arguments` to be `None` if they haven't already
    been set. This is a handy way to handle if the `arguments` key is
    missing

--- src/models/batch.py
+++ src/models/batch.py
@@ -39,7 +39,7 @@ class Batch(Base):
 
     def __init__(self, **kwargs):
         self.config = kwargs['config']
-        if 'arguments' in kwargs: self.arguments = kwargs['arguments']
+        self.arguments = kwargs.setdefault('arguments')
         self.__init_or_load()
 
     @orm.reconstructor
