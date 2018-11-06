263bbfbf47d4c29573d4328907efcdceae23900e
william <william.mccumstie@outlook.com>
2018-11-06 23:21:55 +0000

    Hook the run code into the task
    The `Job` is now run both concurrently and in parallel. The
    `open_connection` method has been transformed to `with_connection`.
    
    This allows for the creation and running over `ssh` to occur in the same
    thread.

--- src/models/job.py
+++ src/models/job.py
@@ -45,9 +45,7 @@ available. Please see documentation for possible causes
             return await self._loop.run_in_executor(None, func, *a)
 
         async def run(self):
-            if self.job.check_command():
-                if await self.__run_thread(self.job.open_connection):
-                    pass
+            if self.job.check_command(): await self.__run_thread(self.job.run)
 
             # Prints the Results
             if self.job.exit_code == 0:
@@ -65,83 +63,66 @@ available. Please see documentation for possible causes
             self.stderr = 'Incorrectly configured command'
             self.exit_code = -2
 
-    def open_connection(self):
+    def with_connection(self, func):
         self.connection = Connection(self.node)
         try:
             self.connection.open()
-            return self.connection
         except:
             self.stdout = ''
             self.stderr = 'Could not establish ssh connection'
             self.exit_code = -1
+        if self.connection.is_connected: return func()
 
     def close_connection(self):
-        self.connection.close()
+        if self.connection: self.connection.close()
 
     def run(self):
-        def __with_connection(func):
-            def wrapper():
-                connection = Connection(self.node)
-                try:
-                    connection.open()
-                except:
-                    self.stderr = 'Could not establish ssh connection'
-                    self.exit_code = -1
-                if connection.is_connected:
+        def __set_result(result):
+            if self.batch.is_interactive():
+                self.stdout = 'Interactive Job: STDOUT is unavailable'
+                self.stderr = 'Interactive Job: STDERR is unavailable'
+                self.exit_code = -3
+            else:
+                self.stdout = result.stdout
+                self.stderr = result.stderr
+                self.exit_code = result.return_code
+
+        def __with_tempdir(func):
+            def wrapper(*args):
+                result = self.connection.run('mktemp -d', hide='both')
+                if result:
+                    temp_dir = result.stdout.rstrip()
                     try:
-                        func(connection)
+                        func(temp_dir, *args)
                     finally:
-                        connection.close()
+                        self.connection.run("rm -rf {}".format(temp_dir))
+                else:
+                    __set_result(result)
             return wrapper
 
-        @__with_connection
-        def __runner(connection):
-            def __set_result(result):
-                if self.batch.is_interactive():
-                    self.stdout = 'Interactive Job: STDOUT is unavailable'
-                    self.stderr = 'Interactive Job: STDERR is unavailable'
-                    self.exit_code = -3
-                else:
-                    self.stdout = result.stdout
-                    self.stderr = result.stderr
-                    self.exit_code = result.return_code
-
-            def __with_tempdir(func):
-                def wrapper(*args):
-                    result = connection.run('mktemp -d', hide='both')
-                    if result:
-                        temp_dir = result.stdout.rstrip()
-                        try:
-                            func(temp_dir, *args)
-                        finally:
-                            connection.run("rm -rf {}".format(temp_dir))
-                    else:
-                        __set_result(result)
-                return wrapper
-
-            @__with_tempdir
-            def __run_command(temp_dir):
-                # Copies the files across
-                parts = [os.path.dirname(self.batch.config), '*']
-                for src_path in glob.glob(os.path.join(*parts)):
-                    result = connection.put(src_path, temp_dir)
-                    if not result:
-                        __set_result(result)
-                        return
-
-                # Runs the command
-                with connection.cd(temp_dir):
-                    kwargs = { 'warn' : True }
-                    if self.batch.is_interactive():
-                        kwargs.update({ 'pty': True })
-                    else:
-                        kwargs.update({ 'hide': 'both' })
-                    cmd = self.batch.command()
-                    if self.batch.arguments: cmd = cmd + ' ' + quote(self.batch.arguments)
-                    result = connection.run(cmd, **kwargs)
+        @__with_tempdir
+        def __run_command(temp_dir):
+            # Copies the files across
+            parts = [os.path.dirname(self.batch.config), '*']
+            for src_path in glob.glob(os.path.join(*parts)):
+                result = self.connection.put(src_path, temp_dir)
+                if not result:
                     __set_result(result)
-            __run_command()
-        __runner()
+                    return
+
+            # Runs the command
+            with self.connection.cd(temp_dir):
+                kwargs = { 'warn' : True }
+                if self.batch.is_interactive():
+                    kwargs.update({ 'pty': True })
+                else:
+                    kwargs.update({ 'hide': 'both' })
+                cmd = self.batch.command()
+                if self.batch.arguments: cmd = cmd + ' ' + quote(self.batch.arguments)
+                result = self.connection.run(cmd, **kwargs)
+                __set_result(result)
+
+        self.with_connection(__run_command)
 
     def __init__(self, **kwargs):
         self.node = kwargs['node']
