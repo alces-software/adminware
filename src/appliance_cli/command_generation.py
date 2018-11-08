42b4acea80191cf945d19660e09c27bb92b29c1e
DavidMarchant <davidwjmarchant@gmail.com>
2018-11-08 17:04:58 +0000

    Make command_generation pass ctx to callback
    If `pass_context` feild is set in config dict

--- src/appliance_cli/command_generation.py
+++ src/appliance_cli/command_generation.py
@@ -131,7 +131,7 @@ def _parse_simple_command_config(ancestor_commands, config, callback):
 
     # Define function to be passed as 'callback' parameter to click.Command,
     # transforming its arguments suitable to be passed to our own callback.
-    def click_callback(**params):
+    def click_callback(ctx, **params):
         argument_values = [
             params[arg_name] for arg_name in arguments.keys()
         ]
@@ -147,11 +147,16 @@ def _parse_simple_command_config(ancestor_commands, config, callback):
         new_commands = deepcopy(ancestor_commands)
         new_options = deepcopy(options)
 
-        callback(new_commands, argument_values, new_options)
+        callback_args = [new_commands, argument_values, new_options]
+
+        if config.get('pass_context'):
+            callback_args += [ctx]
+
+        callback(*callback_args)
 
     return {
         'params': click_params,
-        'callback': click_callback
+        'callback': click.pass_context(click_callback)
     }
 
 
