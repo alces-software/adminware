3246c0bbcc7083585e8adba65347241f00b501da
DavidMarchant <davidwjmarchant@gmail.com>
2018-11-13 12:16:50 +0000

    Remove relevant code from groups & config model
    Also sorted their imports
    And removed config model's re-definistion of tool directory

--- src/groups.py
+++ src/groups.py
@@ -1,31 +1,11 @@
 
 # all dependancy on nodeattr is to be located in this file
 import config
-from models.config import Config
-from appliance_cli.command_generation import generate_commands
 
 from plumbum import local, ProcessExecutionError
 from tempfile import NamedTemporaryFile
 from click import ClickException
 from re import search
-
-def group_commands(root_command, **kwargs):
-    def __group_commands(callback):
-        groups_hash = __hashify_all_groups(**kwargs)
-        generate_commands(root_command, groups_hash, callback)
-    return __group_commands
-
-# Generates a similar hash to Config hasify funcs - for node groups
-#   {
-#       groupX: **<node>,
-#       ...
-#   }
-def __hashify_all_groups(command = {}):
-    combined_hash = {}
-    for group in list_groups():
-        group_hash = combined_hash.setdefault(group, {})
-        Config._Config__copy_values(command, group_hash, group)
-    return combined_hash
 
 def list_groups():
     groups = __nodeattr(arguments=['-l'])
@@ -41,7 +21,7 @@ def expand_nodes(node_list):
     # intercept to generate a more useful error message
     #   before invalid nodenames are caught generically in __nodeattr
     for node in node_list:
-        if search(r'[^A-z0-9,\[\]]',node):
+        if search(r'[^A-z0-9,\[\]]', node):
             raise ClickException("""
 Invalid nodename {} - may only contain alphanumerics, ',', '[' and ']'
 """.strip().format(node))
