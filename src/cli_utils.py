41d54327a23ba90084f73f5acc0441a5167351f2
DavidMarchant <davidwjmarchant@gmail.com>
2018-11-19 12:06:58 +0000

    Fix `cli_utils` encapsulation
    Move from double to single underscore to indicate non-private
    definitions
    See commit: dfa5a2ff0489760079dc93cbbba240cd1ec6efc4

--- src/cli_utils.py
+++ src/cli_utils.py
@@ -19,10 +19,10 @@ hash__node__group = {
 }
 
 def with__node__group(cmd_func):
-    def __with__node__group(ctx, config, argv, options, *a):
+    def _with__node__group(ctx, config, argv, options, *a):
         nodes = nodes_in__node__group(options)
         cmd_func(ctx, config, argv, options, *a, nodes)
-    return __with__node__group
+    return _with__node__group
 
 def nodes_in__node__group(options):
     nodes = groups.expand_nodes(options['node'].value)
@@ -31,14 +31,14 @@ def nodes_in__node__group(options):
         if not group_nodes:
             raise ClickException('Could not find group: {}'.format(group))
         nodes.extend(group_nodes)
-    return list(__remove_duplicates(nodes))
+    return list(_remove_duplicates(nodes))
 
-def __remove_duplicates(target_list):
+def _remove_duplicates(target_list):
     # gone for the slightly more intensive method that preserves order for pretty output
     return OrderedDict((x, True) for x in target_list).keys()
 
 def ignore_parent_commands(func):
-    def __wrapper(ctx, *args, **kwargs):
+    def _wrapper(ctx, *args, **kwargs):
         if not ctx.invoked_subcommand:
             func(ctx, *args, **kwargs)
-    return __wrapper
+    return _wrapper
