74573eebbe4747bf91be269ac899eace19c39080
DavidMarchant <davidwjmarchant@gmail.com>
2018-11-19 12:08:03 +0000

    Fix `command_creator` encapsulation
    Move from double to single underscore to indicate non-private
    definitions
    See commit: dfa5a2ff0489760079dc93cbbba240cd1ec6efc4
    `tools` and `groups`'s access doesn't need to be altered as it is
    specified through their module

--- src/command_creator.py
+++ src/command_creator.py
@@ -28,19 +28,19 @@ No tools found in '{}'
     kwargs['group']['pass_context'] = True
     kwargs['command']['pass_context'] = True
 
-    def __tools(config_callback):
-        config_hash = __hashify_all(subcommand_key = 'commands', **kwargs)
+    def _tools(config_callback):
+        config_hash = _hashify_all(subcommand_key = 'commands', **kwargs)
         callback = ConfigCallback(config_callback).run
         generate_commands(root_command, config_hash, callback)
-    return __tools
+    return _tools
 
 def groups(root_command, **kwargs):
     kwargs['command']['pass_context'] = True
 
-    def __groups(callback):
-        groups_hash = __hashify_all_groups(**kwargs)
+    def _groups(callback):
+        groups_hash = _hashify_all_groups(**kwargs)
         generate_commands(root_command, groups_hash, callback)
-    return __groups
+    return _groups
 
 # The commands are hashed into the following structure
 # NOTES: `command` and `group` both supports callable objects as a means
@@ -57,43 +57,43 @@ def groups(root_command, **kwargs):
 #           }
 #       }
 #   }
-def __hashify_all(group = {}, command = {}, subcommand_key = ''):
+def _hashify_all(group = {}, command = {}, subcommand_key = ''):
     def build_group_hashes():
         cur_hash = combined_hash
         names = config.names()
         for idx, name in enumerate(config.names()):
             cur_names = names[0:idx]
-            __copy_values(group, cur_hash, cur_names)
+            _copy_values(group, cur_hash, cur_names)
             cur_hash = cur_hash.setdefault(subcommand_key, {})\
                                .setdefault(name, {})
         return cur_hash
 
     combined_hash = {}
     for config in glob_configs():
-        __copy_values(command, build_group_hashes(), config)
+        _copy_values(command, build_group_hashes(), config)
 
     return combined_hash.setdefault(subcommand_key, {})
 
 @lru_cache()
-def __glob_paths(*parts):
+def _glob_paths(*parts):
     path = os.path.join(config.TOOL_DIR, *parts, '**/config.yaml')
     return glob(path, recursive=True)
 
 def glob_configs(parts = []):
-    return list(map(lambda p: Config.cache(p), __glob_paths(*parts)))
+    return list(map(lambda p: Config.cache(p), _glob_paths(*parts)))
 
 # Generates a similar hash to Config hasify func - for node groups
 #   {
 #       groupX: **<node>,
 #       ...
 #   }
-def __hashify_all_groups(command = {}):
+def _hashify_all_groups(command = {}):
      combined_hash = {}
      for group in groups_util.list_():
          group_hash = combined_hash.setdefault(group, {})
-         __copy_values(command, group_hash, group)
+         _copy_values(command, group_hash, group)
      return combined_hash
 
-def __copy_values(source, target, args):
+def _copy_values(source, target, args):
    for k, v in source.items():
        target[k] = (v(args) if callable(v) else v)
